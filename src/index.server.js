1a32e4a9b7ac2c21565caa5c386e7ee233ad70ce
Lee-eunjoo-love <hychunjae2025@gmail.com>
2025-11-28 19:55:25 +0900

    [오류 수정] ERROR: Error [ERR_PACKAGE_PATH_NOT_EXPORTED]: Package subpath './server' is not defined by "exports" ~ react-router-dom

--- src/index.server.js
+++ src/index.server.js
@@ -1,7 +1,11 @@
+/** ERROR: Error [ERR_PACKAGE_PATH_NOT_EXPORTED]: Package subpath './server' is not defined by "exports" ~ react-router-dom */
+// #. { StaticRouter } react-router-dom/server => react-router-dom 으로 변경
 import ReactDOMServer from "react-dom/server";
 import express from "express";
-import { StaticRouter } from "react-router-dom/server";
+//import { StaticRouter } from "react-router-dom/server"; // #. Error [ERR_PACKAGE_PATH_NOT_EXPORTED]
+import { StaticRouter } from "react-router-dom";
 import App from "./App";
+import path from "path"; // #. 정정 파일 제공하기
 
 const app = express();
 
@@ -16,16 +20,21 @@ const serverRender = (req, res, next) => {
   res.send(root);
 };
 
+// #. 정적 파일 제공하기
+const serve = express.static(path.resolve("./build"), { index: false }); // #. 경로에서 index.html 보여주지 않음 설정
+app.use(serve);
+
 app.use(serverRender);
 
 app.listen(5000, () => {
   console.log("Running on http://localhost:5000");
 });
 
-/*import ReactDOMServer from "react-dom/server";
+/*
+import ReactDOMServer from "react-dom/server";
 
 const html = ReactDOMServer.renderToString(
   <div>Hello Server Side Rendering!</div>
 );
 
-console.log(html);*/
+console.log(html); //*/
